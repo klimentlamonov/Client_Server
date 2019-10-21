071b5d7ae01687a6d963c27f7e7daebd0dcb33c8
root <root@localhost.localdomain>
2019-10-21 23:49:19 +0300

    Add Server-Code

---
+++ Server.java
@@ -0,0 +1,45 @@
+package lka.server.Client_Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.lang.System.out;
+
+public class Server {
+    public static void main(String[] args) {
+
+        try (ServerSocket server = new ServerSocket(8000))
+        {
+            out.println("Server started");
+
+            while (true)
+            try(
+                    Socket socket = server.accept();
+
+                    BufferedWriter writer =
+                            new BufferedWriter(
+                                    new OutputStreamWriter(
+                                            socket.getOutputStream()));
+
+                    BufferedReader reader =
+                            new BufferedReader(
+                                    new InputStreamReader(
+                                            socket.getInputStream()));
+                    ) {
+                String requets = reader.readLine();
+                out.println("Request is: " + requets);
+
+                String response = "Hello from server!";
+                out.println("Response: " + response);
+
+                writer.write(response);
+                writer.newLine();
+                writer.flush();
+            }
+
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
