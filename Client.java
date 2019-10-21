906f3198191bae39599cc93a0c1e602254a1bc2b
root <root@localhost.localdomain>
2019-10-21 23:51:55 +0300

    Add Client Code

---
+++ Client.java
@@ -0,0 +1,36 @@
+package lka.client.Client_Server;
+
+import java.io.*;
+import java.net.Socket;
+
+import static java.lang.System.out;
+
+public class Client {
+    public static void main(String[] args) {
+        try (
+                Socket socket = new Socket("127.0.0.1", 8000);
+                BufferedWriter writer =
+                        new BufferedWriter(
+                                new OutputStreamWriter(
+                                        socket.getOutputStream()));
+
+                BufferedReader reader =
+                        new BufferedReader(
+                                new InputStreamReader(
+                                        socket.getInputStream()));
+                ) {
+            out.println("Connected to server!");
+            String request = "Knock - knock";
+            out.println("Request: " + request);
+
+            writer.write(request);
+            writer.newLine();
+            writer.flush();
+
+            String response = reader.readLine();
+            out.println("Response: " + response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
